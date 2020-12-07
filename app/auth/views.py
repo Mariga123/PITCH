3873f5ed09e1e8a117db2091882dd0031cec6653
Mariga123 <johnmariga8@gmail.com>
2020-12-07 10:36:00 +0300

    create a route for auth logout and sign up form

--- app/auth/views.py
+++ app/auth/views.py
@@ -17,3 +17,21 @@ def login():
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or Password')
     return render_template('auth/login.html', loginform=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
+
+
+@auth.route('/signup', methods=["GET", "POST"])
+def signup():
+    form = RegForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        user.save_u()
+        mail_message("Welcome to Pitch-World", "email/welcome_user", user.email, user=user)
+        return redirect(url_for('auth.login'))
+    return render_template('auth/signup.html', r_form=form)
