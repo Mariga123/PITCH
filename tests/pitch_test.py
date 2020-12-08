994bf7d9f9fbc8b027944800cba8861ae8df942b
Mariga123 <johnmariga8@gmail.com>
2020-12-08 06:22:34 +0300

    add a test class for pitch

---
+++ tests/pitch_test.py
@@ -0,0 +1,30 @@
+import unittest
+from app.models import Pitch,User
+from app import db
+
+class PitchTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Pitch class
+    '''
+    def setUp(self):
+        self.user_James = User(username = 'mariga',password = 'ze11yjones', email = 'johnmariga8@gmail.com')
+        self.new_comment = Comment(pitch_id=12345,pitch_title='create comment',comment_pitch='web developers are the best',user = self.user_mariga)
+
+    def tearDown(self):
+        Pitch.query.delete()
+        User.query.delete()
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_pitch.pitch,'An opening forum for youths')
+        self.assertEquals(self.new_pitch.user,self.user_James)
+
+    def test_save_pitch(self):
+        self.new_pitch.save_pitch()
+        self.assertTrue(len(Pitch.query.all())>0)
+
+    def test_get_pitch_by_id(self):
+
+        self.new_pitch.save_pitch()
+        get_pitches = Pitch.get_all_piches()
+        self.assertTrue(len(got_pitches) == 1)
+
